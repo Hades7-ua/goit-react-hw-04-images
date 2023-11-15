8ce186439fce253ff713270c9e03d535ffe98f93
Vitalii Tkachov <tkachov0703@gmail.com>
2023-11-15 23:35:55 +0300

    API request

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,38 @@
 import { Component } from 'react';
+import { fetchImages } from 'services/api';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 
 export class App extends Component {
   state = {
     images: [],
+    isLoading: false,
   };
+  async componentDidMount() {
+    try {
+      this.setState({ isLoading: true });
+      const initialImages = await fetchImages();
+      // console.log(initialImages);
+      this.setState({ images: initialImages.hits });
+    } catch (error) {
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
   render() {
+    const { images, isLoading } = this.state;
+
     return (
       <div>
-        <form></form>
-        <div>Gallery</div>
-        <button>Load More</button>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <form>{/* ваша форма */}</form>
+            <ImageGalleryItem items={images} />
+            <button>Load More</button>
+          </>
+        )}
       </div>
     );
   }
 }
-export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
+
 axios.defaults.baseURL =
   'https://pixabay.com/api/?q=cat&page=1&key=37999116-3b925aa00aefce89ac64d9c93&image_type=photo&orientation=horizontal&per_page=12';
 
-export const fetchImages = async () => {
-  const responce = await axios.get('images');
-  return responce.data;
+export const fetchImages = async (page = 1, perPage = 12) => {
+  try {
+    const response = await axios.get('images', {
+      params: {
+        page: page,
+        per_page: perPage,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
 };
